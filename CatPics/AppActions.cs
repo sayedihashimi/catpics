25f2cf52398dd27c156733132f7209a00088d354
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-06-27 12:15:32 -0400

    changes to start implementing Actions, wip. Updated Xam.Forms

---
+++ CatPics/AppActions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatPics {
+    public class AppActions {
+
+        public List<CustomDisplayAction> GetCatImageAppActions(){
+            throw new NotImplementedException();
+        }
+
+        //private List<TemplateDisplayAction> GetTemplatePackActions() {
+        //    List<TemplateDisplayAction> actions = new List<TemplateDisplayAction>
+        //    {
+        //        new TemplateDisplayAction("View on nuget.org",()=>ExecuteBrowseToNuget(null), true),
+        //        new TemplateDisplayAction("Go to project site",()=>ExecuteBrowseProjectSite(), !string.IsNullOrWhiteSpace(ProjectUrl)),
+        //        new TemplateDisplayAction("View license",()=>ExecuteBrowseToLicense(null), !string.IsNullOrWhiteSpace(LicenseUrl) ),
+        //        new TemplateDisplayAction("Share",()=>ExecuteShare(), true)
+        //    };
+
+        //    return actions;
+        //}
+    }
+}
